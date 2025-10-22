05e4edb770f6170be0c6dc7bd968cc39d70e7b5c
EleazarDev <eleazarjarquincv@gmail.com>
2025-10-22 16:30:09 -0600

    feat: Se agrega app router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+// Se importan las páginas
+import Layout from "./components/layout/Layout";
+import Home from "./pages/home/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      {/* Trabajaremos con redireccionamiento de páginas con react-router */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* Rutas hijas */}
+          <Route index element={<Home />} />
+          {/* Aquí van las rutas de las pages */}
+          {/* Ejemplo: <Route path="tareas" element={<TodoList />} /> */}
+
+          {/* Aquí van las rutas de errores (404, 405, etc..) */}
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
